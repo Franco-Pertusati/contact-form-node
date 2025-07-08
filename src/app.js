8ab3b3524147803f2e3e37accde06814f817f2da
Franco Pertusati <pertusatifranco836@gmail.com>
2025-07-08 15:15:10 -0300

    Primera version funcional

---
+++ src/app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const transporter = require("./utils/transporter");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/api/contact/send", function (req, res) {
+  const { text, subject, name, company } = req.body
+
+  transporter.sendMail(
+    {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL_RECEIVER,
+      subject: `Mensaje de portfolio - ${subject}`,
+      text: `
+        Nuevo mensaje desde el formulario de contacto:
+
+        Nombre: ${name}
+        Empresa: ${company}
+        Mensaje: ${text}`,
+    },
+    function (error, info) {
+      if (error) {
+        console.error("Error al enviar el correo:", error);
+        return res
+          .status(500)
+          .json({ message: "Error al enviar el correo", error });
+      } else {
+        console.log("Correo enviado:", info.response);
+        return res
+          .status(200)
+          .json({ message: "Correo enviado correctamente", info });
+      }
+    }
+  );
+});
+
+module.exports = app;
