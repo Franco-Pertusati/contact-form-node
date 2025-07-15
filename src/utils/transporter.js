6adae9d930f68513593afd3bad3a2eef03de5237
Franco Pertusati <pertusatifranco836@gmail.com>
2025-07-15 12:22:53 -0300

    correccion en la lectura del .env

--- src/utils/transporter.js
+++ src/utils/transporter.js
@@ -1,14 +1,14 @@
 const nodemailer = require("nodemailer");
-require('dotenv').config();
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: process.env.SECURE_CONNECTION,
+  secure: process.env.SECURE_CONNECTION !== "false",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
   },
 });
 
-module.exports = transporter
+module.exports = transporter;
