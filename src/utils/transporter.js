98ac097503238207dfed2f6bfc8661aef110a02d
Franco Pertusati <pertusatifranco836@gmail.com>
2025-07-15 11:24:28 -0300

    Añadida variable de HOST del email

--- src/utils/transporter.js
+++ src/utils/transporter.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   secure: true,
   auth: {
