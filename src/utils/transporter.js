42782faadb59bee55523b7719224062111f193ab
Franco Pertusati <pertusatifranco836@gmail.com>
2025-07-15 12:01:51 -0300

    Variable de entorno de el tipo de coneccoin

--- src/utils/transporter.js
+++ src/utils/transporter.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: true,
+  secure: process.env.SECURE_CONNECTION,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
